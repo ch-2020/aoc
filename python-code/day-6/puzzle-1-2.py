58843ebc6bf4bc6f1323de58e50bcb061e826150
chkoo <cheehung.koo@gmail.com>
2023-12-06 08:58:00 +0100

    day6 part1 and 2

--- python-code/day-6/puzzle-1-2.py
+++ python-code/day-6/puzzle-1-2.py
@@ -1,54 +1,80 @@
 import sys, os
-import re
+import numpy as np
 
 sys.path.append("..")
 from funcs.readfile import ReadFileObject 
 
-# TO DO
-class TOBEDEFINE:
-    def __init__(self) -> None:
-        # TO DO
-        pass
+class BoatRace:
+    def __init__(self, inputs) -> None:
+        self.inputs = inputs
+        self.matches = []
 
     def run_program(self, mode) -> int:
         if mode == "part1":
-        # TO DO
-            pass
-            
+            self.part1_generate_items()
+            return self.solution()
+              
         elif mode == "part2":
-        # TO DO
-            pass
+            self.part2_generate_items()
+            return self.solution()
+            
+
+    def part2_generate_items(self):
+        time = int("".join(list(self.inputs[0].split(":")[1].split())))
+        dist = int("".join(list(self.inputs[1].split(":")[1].split())))
+
+        self.matches.append([time, dist])
+
+    def part1_generate_items(self):
+        times = list(map(int, self.inputs[0].split(":")[1].split()))
+        dists = list(map(int, self.inputs[1].split(":")[1].split()))
+        for i in range(0, len(times)):
+            self.matches.append([times[i], dists[i]])
+    
+    def solution(self): 
+        success_tries = []
+        total_sum = 0
+        for match in self.matches:
+            count_benchmark = 0
+            t = match[0]
+            d = match[1]
+
+            for p in range(0, t + 1):
+                dist = (t - p) * (p)
+                if dist <= d:
+                    count_benchmark += 1
+                else:
+                    break
+            success_num = (t + 1) - 2 * count_benchmark
+            success_tries.append(success_num)
+  
+        total_sum = np.prod(np.array(success_tries))  
+        print(f'{success_tries}: {total_sum}')
+        return total_sum
+
 
 if __name__ == "__main__":
-    mode = "test" #"part2", "test"
+    mode = "part2" #"part2", "test"
 
     if mode == "test":
-        # TO DO
-        res_test1 = {0: {0: 'X', 1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 1: {0: 'X', 1: 'X', 2: 'X', 3: '*', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 2: {0: 'X', 1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 3: {0: 'X', 1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: '#', 7: 'X', 8: 'X', 9: 'X'}, 4: {0: 'X', 1: 'X', 2: 'X', 3: '*', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 5: {0: 'X', 1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: '+', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 6: {0: 'X', 1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 7: {0: 'X', 1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 8: {0: 'X', 1: 'X', 2: 'X', 3: '$', 4: 'X', 5: '*', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}, 9: {0: 'X', 1: 'X', 2: 'X', 3: 'X', 4: 'X', 5: 'X', 6: 'X', 7: 'X', 8: 'X', 9: 'X'}}
-     
-        test_obj = TOBEDEFINE()
+        test_filepath = os.path.join(os.path.dirname(__file__), 'puzzle-test.txt')
+        test_lines = ReadFileObject().get_lines_from_file(test_filepath)
+        
+        test_obj = BoatRace(test_lines[1])
         unittest_dataprocessing = { 
-            'test_1': res_test1 == test_obj.run_program("part1")
+            'test_1': 288 == test_obj.run_program("part1")
             }
         for k, v in unittest_dataprocessing.items():
             print(f'{k}: {"passed" if v else "failed"}')
 
     else:
-        # TO DO
-        filepath = os.path.join(os.path.dirname(__file__), 'puzzle-day-TODO-input.txt')
+        filepath = os.path.join(os.path.dirname(__file__), 'puzzle-day-6-input.txt')
         lines = ReadFileObject().get_lines_from_file(filepath)
         
         if lines[0]:
-            func_obj = TOBEDEFINE(lines[1])
-            # TO DO
-
-            if mode == "part1": 
-                # TO DO
-                print(f'Total number is {sum}')
-
-            if mode == "part2":
-                # TO DO
-                print(f'Total number is {sum}')
+            func_obj = BoatRace(lines[1])
+            sum = func_obj.run_program(mode)
+            print(f'Total number is {sum}')
 
         else:
             print("Error when opening file!")
