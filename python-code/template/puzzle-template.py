7d828d60d12a7a1dfcf8012934fbcd0f0ec5e80d
chkoo <cheehung.koo@gmail.com>
2023-12-11 08:18:19 +0100

    refactoring and updated template

--- python-code/template/puzzle-template.py
+++ python-code/template/puzzle-template.py
@@ -1,13 +1,18 @@
 import numpy as np
-import pprint
+import matplotlib
+matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+from matplotlib import colors
 
-class PipeMaze:
+class TODO:
     def __init__(self, filepath) -> None:
         self.filepath = filepath
         self.inputs = []
-        self.sum = 0
+        self.extracted_data = []
 
     def run_program(self, mode) -> int:
+        self.extract_data()
+       
         if mode == "part1":
             self.sum = self.part1_solution()
             
@@ -19,18 +24,22 @@ class PipeMaze:
     def extract_data(self):
         with open(self.filepath, "r") as f:
             self.inputs = f.readlines()
+            for l in self.inputs:
+                puz = [x for x in [*l]]
+                self.extracted_data.append(puz)
 
     def part1_solution(self) -> int:
-        pass
+       pass
+
 
     def part2_solution(self) -> int:
         pass
 
 if __name__ == "__main__":
-    mode = "test" #"part2", "test"
+    mode = "part1" #"part2", "test"
 
     if mode == "test":     
-        test_obj = PipeMaze("puzzle-test.txt")
+        test_obj = TODO("puzzle-test.txt")
         unittest_dataprocessing = { 
             'test_1': 8 == test_obj.run_program("part1")
             }
@@ -38,6 +47,6 @@ if __name__ == "__main__":
             print(f'{k}: {"passed" if v else "failed"}')
 
     else:
-        func_obj = PipeMaze("puzzle-day-10-input.txt")      
+        func_obj = TODO("puzzle-day-10-input.txt")      
         sum = func_obj.run_program(mode)    
         print(f'Total number is {sum}')
