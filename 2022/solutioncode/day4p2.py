2c29e0132b8d8a4d0d870bc3b55491488ac0e49a
ch-2020 <cheehung.koo@gmail.com>
2024-02-05 14:06:06 +0100

    d4p12 & d5p1

--- 2022/solutioncode/day4p2.py
+++ 2022/solutioncode/day4p2.py
@@ -1,2 +1,12 @@
 from global_ import globalvariables as gv
 f = gv.filecontent
+
+sum = 0
+for l in f.split("\n"):
+    both = l.split(",")
+    r1f, r1e = map(int, both[0].split("-"))
+    r2f, r2e = map(int, both[1].split("-"))
+    if (r1f >= r2f and r1f <= r2e) or (r2f >= r1f and r2f <= r1e):
+        sum += 1
+
+print(sum)
