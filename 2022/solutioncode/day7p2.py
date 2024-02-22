748d22be18c7cd7c5abd7d152b6bd864a54386fe
ch-2020 <cheehung.koo@gmail.com>
2024-02-22 14:34:41 +0100

    d7p2

--- 2022/solutioncode/day7p2.py
+++ 2022/solutioncode/day7p2.py
@@ -1,2 +1,60 @@
 from global_ import globalvariables as gv
 f = gv.filecontent
+
+cwd = root = {}
+stack = []
+for line in f.split("\n"):
+    line = line.strip()
+    if line[0] == "$":
+        if line[2] == "c":
+            dir = line[5:]
+            if dir == "/":
+                cwd = root
+                stack = []
+            elif dir == "..":
+                cwd = stack.pop()
+            else:
+                if dir not in cwd:
+                    cwd[dir] = {}
+                stack.append(cwd)
+                cwd = cwd[dir]
+    else:
+        x, y = line.split()
+        if x == "dir":
+            if y not in cwd:
+                cwd[y] = {}
+        else:
+            cwd[y] = int(x)
+
+def solve(dir = root):
+    if type(dir) == int:
+        return (dir, 0)
+    size = 0
+    ans = 0
+    for child in dir.values():
+        s, a = solve(child)
+        size += s
+        ans += a
+    if size <= 100000:
+        ans += size
+    return (size, ans)
+
+total, sol = solve()
+remaining = 70000000 - total
+required = 30000000 - remaining
+
+d_sum = []
+def findsmallest(dir = root):
+    if type(dir) == int:
+        return (dir, 0)
+    size = 0
+    ans = 0
+    for child in dir.values():
+        s, a = findsmallest(child)
+        size += s
+    if size >= required:
+        d_sum.append(size)
+    return (size, ans)
+
+findsmallest()
+print(min(d_sum))
