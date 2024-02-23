824e4d18bcfdaa7491ac930f202901ed48347203
ch-2020 <cheehung.koo@gmail.com>
2024-02-23 10:26:59 +0100

    d8p1

--- 2022/solutioncode/day8p1.py
+++ 2022/solutioncode/day8p1.py
@@ -14,8 +14,36 @@ for r in range(1, rows):
     seen.add((r, 0))
     seen.add((r, cols - 1))
 
-# cont...
+def find_largest_num(s):
+    max = (0, int(s[0]))
+    res = [max]
+    for i, v in enumerate(s):
+        if int(v) > max[1]:
+            res.append((i, int(v)))
+            max = (i, int(v))
+    return max, res
 
+for r in range(1, rows-1):
+    m, res = find_largest_num(lines[r][:-1])
+    for i in res:
+        seen.add((r, i[0]))
+
+    rev = lines[r][::-1]
+    m, res = find_largest_num(rev[:-1])
+    for i in res:
+        seen.add((r, cols - 1 - i[0]))
+
+otherdir = ["".join(ele) for ele in zip(*lines)]
+
+for c in range(1, cols - 1):
+    m, res = find_largest_num(otherdir[c][:-1])
+    for i in res:
+        seen.add((i[0], c))
+    
+    rev = otherdir[c][::-1]
+    m, res = find_largest_num(rev[:-1])
+    for i in res:
+        seen.add((rows - 1 - i[0], c))
 
 print(sorted(seen))
 print(len(seen))
